b00ee228e04bab8d1f41882beae88fdd393e982e
sarah boo <srhboo@gmail.com>
2020-10-28 00:17:19 -0400

    more comments

--- src/index.js
+++ src/index.js
@@ -7,33 +7,35 @@ import { LineGeometry } from "./jsm/LineGeometry";
 import { LineMaterial } from "./jsm/LineMaterial";
 import { Line2 } from "./jsm/Line2";
 
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-// the points of the main path
+// this is how many points will be created on one half
+// (total will be double this)
 const MAX_POINTS = 100;
-
-// args: FOV, aspect, near plane, far plane
-const camera = new THREE.PerspectiveCamera(40, width / height, 1, 10000);
+// create camera. args: FOV, aspect, near plane, far plane
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  1,
+  10000
+);
+// move camera back to see (0, 0)
 camera.position.z = 10;
-
+// create scene and set background to black
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
 
-let lineLengths = [];
-let lines = [];
+let pathLine1, pathLine2;
+let pathLength;
 let pipePoints = [];
 
-// this is used to create the main path lines
-const createNewLine = (colors, positions, linewidth = 0.002) => {
-  let geometry = new LineGeometry();
-
+// this fn is used to create the main path lines
+const createNewPathLine = (colors, positions, linewidth = 0.002) => {
+  const geometry = new LineGeometry();
   geometry.setPositions(positions);
   geometry.setColors(colors);
 
   // a dashed line is used as a workaround to be able to
   // animate the line growing outwards
-  let matLine = new LineMaterial({
+  const matLine = new LineMaterial({
     color: 0xffffff,
     linewidth,
     vertexColors: true,
@@ -43,6 +45,56 @@ const createNewLine = (colors, positions, linewidth = 0.002) => {
     dashScale: 1,
     transparent: true,
     opacity: 0.8,
+  });
+  const line = new Line2(geometry, matLine);
+  scene.add(line);
+  line.computeLineDistances();
+  line.material.defines.USE_DASH = "";
+  line.material.needsUpdate = true;
+  return line;
+};
+
+// this is for the path reversal lines
+const createNewReversalLineAt = (x, y, z, axis) => {
+  // currently hardcoded to red, but can use this later
+  const colorByAxis = {
+    x: 0xc7170a,
+    y: 0x3449eb,
+    z: 0xfaf60a,
+  };
+  let positionsTemp = new Float32Array(6);
+  let colorsTemp = [];
+
+  // small offset is added to avoid overlapping lines glitching
+  // starting point, along the main path line
+  positionsTemp[0] = x + 0.001;
+  positionsTemp[1] = y + 0.001;
+  positionsTemp[2] = z + 0.001;
+
+  // end point, 2 units away along the corresponding axis
+  positionsTemp[3] = x + 0.001 + (axis === "z" ? 2 : 0);
+  positionsTemp[4] = y + 0.001 + (axis === "x" ? 2 : 0);
+  positionsTemp[5] = z + 0.001 + (axis === "y" ? 2 : 0);
+
+  // possibility for future: can vary colour along
+  // progression of the line
+  for (let i = 0; i < positionsTemp.length; i++) {
+    let c = new THREE.Color(colorByAxis["x"]);
+    colorsTemp.push(c.r, c.g, c.b);
+  }
+  let geometry = new LineGeometry();
+
+  geometry.setPositions(positionsTemp);
+  geometry.setColors(colorsTemp);
+
+  let matLine = new LineMaterial({
+    color: 0xffffff,
+    linewidth: 0.0012,
+    vertexColors: true,
+    dashed: true,
+    dashSize: 0.01,
+    gapSize: 0.01,
+    dashScale: 0,
   });
   const line = new Line2(geometry, matLine);
   scene.add(line);
@@ -60,12 +112,12 @@ const createPlaneAt = (x, y, z, axis) => {
   });
   var plane = new THREE.Mesh(geometry, material);
 
-  // get radians to rotate planes
+  // convert 90 deg angle to radians
   let square = THREE.MathUtils.degToRad(90);
-  let neg = THREE.MathUtils.randInt(0, 1);
 
   // randomly assign the plane to one of the halves of the world
   // aka one of the reflections across each axis
+  let neg = THREE.MathUtils.randInt(0, 1);
   let mult = neg === 1 ? 1 : -1;
 
   // depending on which axis the red lines are formed from,
@@ -120,53 +172,6 @@ const createTubeAt = (lineLength) => {
   var mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
 };
-
-// this is for the path reversal lines
-const createNewLineAt = (x, y, z, axis) => {
-  // currently hardcoded to red, but can use this later
-  const colorByAxis = {
-    x: 0xc7170a,
-    y: 0x3449eb,
-    z: 0xfaf60a,
-  };
-  let positionsTemp = new Float32Array(6);
-  let colorsTemp = [];
-
-  // small offset to avoid overlapping lines glitching
-  positionsTemp[0] = x + 0.001;
-  positionsTemp[1] = y + 0.001;
-  positionsTemp[2] = z + 0.001;
-  positionsTemp[3] = x + 0.001 + (axis === "z" ? 2 : 0);
-  positionsTemp[4] = y + 0.001 + (axis === "x" ? 2 : 0);
-  positionsTemp[5] = z + 0.001 + (axis === "y" ? 2 : 0);
-
-  for (let i = 0; i < positionsTemp.length; i++) {
-    let c = new THREE.Color(colorByAxis["x"]);
-    colorsTemp.push(c.r, c.g, c.b);
-  }
-  let geometry = new LineGeometry();
-
-  geometry.setPositions(positionsTemp);
-  geometry.setColors(colorsTemp);
-
-  let matLine = new LineMaterial({
-    color: 0xffffff,
-    linewidth: 0.0012,
-    vertexColors: true,
-    dashed: true,
-    dashSize: 0.01,
-    gapSize: 0.01,
-    dashScale: 0,
-  });
-  const line = new Line2(geometry, matLine);
-  line.computeLineDistances();
-  line.scale.set(1, 1, 1);
-  line.material.defines.USE_DASH = "";
-  line.material.needsUpdate = true;
-  scene.add(line);
-  return line;
-};
-
 // positions of first main line
 let positions = new Float32Array(MAX_POINTS * 3.0);
 // positions of second main line
@@ -198,13 +203,13 @@ for (let i = 0, l = MAX_POINTS; i < l; i++) {
   positions[index++] = y;
   positions[index++] = z;
 
-  var color = new THREE.Color(0xffffff);
+  const color = new THREE.Color(0xffffff);
   colors.push(color.r, color.g, color.b);
-
   points.push(new THREE.Vector3(x, y, z));
 
-  // the following determines the random point's direction vector
+  // randomize x, y, or z direction
   let dimension = THREE.MathUtils.randInt(0, 2);
+  // randomize positive or negative direction
   let direction = THREE.MathUtils.randInt(0, 1) === 0 ? 1 : -1;
   // hard coded to 1
   let magnitude = THREE.MathUtils.randInt(1, 1);
@@ -220,9 +225,10 @@ for (let i = 0, l = MAX_POINTS; i < l; i++) {
       let xDir = direction * magnitude;
       if (xDir * lastXDir !== 0 && xDir * lastXDir < 0) {
         // create path reversal line if new vector
-        // is opposite of last
-        createNewLineAt(x, y, z, "x");
-        createNewLineAt(-x, -y, -z, "x");
+        // is the exact opposite of last
+        // one for each reflection
+        createNewReversalLineAt(x, y, z, "x");
+        createNewReversalLineAt(-x, -y, -z, "x");
         lastRevisions.push("x");
         // this checks if the last two reversals are in the
         // same direction, and if a plane should be created
@@ -242,8 +248,8 @@ for (let i = 0, l = MAX_POINTS; i < l; i++) {
     case 1:
       let yDir = direction * magnitude;
       if (yDir * lastYDir !== 0 && yDir * lastYDir < 0) {
-        createNewLineAt(x, y, z, "y");
-        createNewLineAt(-x, -y, -z, "y");
+        createNewReversalLineAt(x, y, z, "y");
+        createNewReversalLineAt(-x, -y, -z, "y");
         lastRevisions.push("y");
         if (
           lastRevisions[lastRevisions.length - 1] ===
@@ -258,8 +264,8 @@ for (let i = 0, l = MAX_POINTS; i < l; i++) {
     case 2:
       let zDir = direction * magnitude;
       if (zDir * lastZDir !== 0 && zDir * lastZDir < 0) {
-        createNewLineAt(x, y, z, "z");
-        createNewLineAt(-x, -y, -z, "z");
+        createNewReversalLineAt(x, y, z, "z");
+        createNewReversalLineAt(-x, -y, -z, "z");
         lastRevisions.push("z");
         if (
           lastRevisions[lastRevisions.length - 1] ===
@@ -280,37 +286,27 @@ for (let i = 0, l = MAX_POINTS; i < l; i++) {
       lineDistances[i - 1] + points[i - 1].distanceTo(points[i]);
   }
 }
-lineLengths[0] = lineDistances[MAX_POINTS - 1];
+pathLength = lineDistances[MAX_POINTS - 1];
 
 // create the path line
-let mainLine = createNewLine(colors, positions, 0.0014);
-mainLine.computeLineDistances();
-mainLine.material.defines.USE_DASH = "";
-mainLine.material.needsUpdate = true;
-lines.push(mainLine);
-
+pathLine1 = createNewPathLine(colors, positions, 0.0014);
 // create its reflection
-let mainLine2 = createNewLine(colors, positions2, 0.0014);
-mainLine2.computeLineDistances();
-mainLine2.material.defines.USE_DASH = "";
-mainLine2.material.needsUpdate = true;
-lines.push(mainLine2);
+pathLine2 = createNewPathLine(colors, positions2, 0.0014);
 
 // create the tube
-createTubeAt(lineLengths[0]);
+createTubeAt(pathLength);
 
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(width, height);
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000, 0.0);
 
 // note: orbitcontrols have custom edit to allow zoom by FOV instead of dollying
 // which is needed because by default it changed position to zoom
 // which doesn't work when the camera is moving
 const controls = new OrbitControls(camera, renderer.domElement);
-
 controls.minDistance = 1;
 controls.maxDistance = 1000;
 controls.enablePan = true;
@@ -324,11 +320,11 @@ const animate = () => {
 
   // workaround to animate the main lines
   fraction = fraction + 0.0001 >= 1 ? 1 : fraction + 0.0003; // fraction in [ 0, 1 ]
-  lines[0].material.dashSize = fraction * lineLengths[0];
-  lines[0].geometry.attributes.position.needsUpdate = true;
+  pathLine1.material.dashSize = fraction * pathLength;
+  pathLine1.geometry.attributes.position.needsUpdate = true;
 
-  lines[1].material.dashSize = fraction * lineLengths[0];
-  lines[1].geometry.attributes.position.needsUpdate = true;
+  pathLine2.material.dashSize = fraction * pathLength;
+  pathLine2.geometry.attributes.position.needsUpdate = true;
 
   // animate the camera path
   var time = -performance.now() * 0.0003;
